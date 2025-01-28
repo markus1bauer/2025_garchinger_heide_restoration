f304f53a08703a1bbe689dad4449c5210d15e872
markus1bauer <markus1.bauer@tum.de>
2025-01-28 11:55:17 +0100

    update model calculation species richness

--- R/model_calculation_species_richness.R
+++ R/model_calculation_species_richness.R
@@ -33,7 +33,8 @@ sites <- read_csv(
     cols(
       .default = "?"
     )
-)
+) %>%
+  rename(y = richness_total)
 
 
 
@@ -46,84 +47,74 @@ sites <- read_csv(
 ## 1 Data exploration ##########################################################
 
 
+## 1 Data exploration ##########################################################
+
+
 ### a Graphs of raw data -------------------------------------------------------
 
-# plot1 <- ggplot(sites %>% filter(survey_year == 2021),
-#                 aes(y = n, x = sand_ratio)) +
-#   geom_quasirandom(color = "grey") + geom_boxplot(fill = "transparent") +
-#   facet_grid(~ survey_year_fct) +
-#   labs(title = "Sand ratio [vol%]")
+ggplot(sites, aes(y = y, x = treatment)) +
+  geom_quasirandom(color = "grey") + geom_boxplot(fill = "transparent")
+ggplot(sites, aes(y = y, x = height_vegetation)) +
+  geom_quasirandom(color = "grey") + geom_smooth(method = "lm") +
+  facet_grid(~treatment)
+ggplot(sites, aes(y = y, x = cover_vegetation)) +
+  geom_quasirandom(color = "grey") + geom_smooth(method = "lm") +
+  facet_grid(~treatment)
 
-plot1 <- ggplot(sites, aes(y = richness_total, x = treatment)) +
-  geom_quasirandom(color = "grey") + geom_boxplot(fill = "transparent");plot1
 
 ### b Outliers, zero-inflation, transformations? ----------------------------
 
-# sites %>% group_by(exposition) %>% count(site)
-# boxplot(sites$n)
-# ggplot(sites, aes(x = exposition, y = n)) + geom_quasirandom()
-# ggplot(sites, aes(x = n)) + geom_histogram(binwidth = 0.03)
-# ggplot(sites, aes(x = n)) + geom_density()
-
 sites %>% group_by(treatment) %>% count(treatment)
-boxplot(sites$richness_total)
-ggplot(sites, aes(x = treatment, y = richness_total)) + geom_quasirandom()
-ggplot(sites, aes(x = richness_total)) + geom_histogram(binwidth = 1)
-ggplot(sites, aes(x = richness_total)) + geom_density()
+ggplot(sites, aes(x = treatment, y = y)) + geom_quasirandom()
+ggplot(sites, aes(x = y)) + geom_histogram(binwidth = 1)
+ggplot(sites, aes(x = y)) + geom_density()
+
 
 ### c Check collinearity ------------------------------------------------------
 
-# sites %>%
-#   select(where(is.numeric), -b, -c, -d, -y, -ends_with("scaled")) %>%
-#   GGally::ggpairs(lower = list(continuous = "smooth_loess")) +
-#   theme(strip.text = element_text(size = 7))
-# sites <- sites %>%
-#   select(-biotope_area)
+sites %>%
+  select(height_vegetation, cover_vegetation) %>%
+  GGally::ggpairs(lower = list(continuous = "smooth_loess")) +
+  theme(strip.text = element_text(size = 7))
 #--> exclude r > 0.7
 # Dormann et al. 2013 Ecography
 # https://doi.org/10.1111/j.1600-0587.2012.07348.x
 
-sites %>%
-  select(where(is.numeric), -(4:12), -14) %>%
-  GGally::ggpairs(lower = list(continuous = "smooth_loess")) +
-  theme(strip.text = element_text(size = 7))
-sites <- sites %>%
-  select(-shannon)
-
 
 ## 2 Model building ###########################################################
+
 
 ### a Random structure ---------------------------------------------------------
 
 m1a <- blmer(
-  richness_total ~ 1 + (1 | patch), data = sites, REML = TRUE
+  y ~ 1 + (1 | patch), data = sites, REML = TRUE
 )
 
-MuMIn::AICc(m1a, m1b, m1c) %>%
+MuMIn::AICc(m1a) %>%
   arrange(AICc)
 
 
 ### b Fixed effects ------------------------------------------------------------
 
 m1 <- blmer(
-  richness_total ~ treatment,
+  y ~ treatment + (1 | patch),
   REML = FALSE,
   control = lmerControl(optimizer = "Nelder_Mead"),
   cov.prior = wishart,
   data = sites
 )
+
 m1 <- lm(
-  richness_total ~ treatment,
+  y ~ treatment,
   data = sites
 )
 simulateResiduals(m1, plot = TRUE)
 
 m2 <- lm(
-  richness_total ~ treatment + cover_vegetation,
+  y ~ treatment + cover_vegetation,
   data = sites
 )
 simulateResiduals(m2, plot = TRUE)
-
 
 
 ### d Save ---------------------------------------------------------------------
@@ -131,4 +122,3 @@ simulateResiduals(m2, plot = TRUE)
 
 save(m1, file = here("outputs", "models", "model_species_richness_1.Rdata"))
 save(m2, file = here("outputs", "models", "model_species_richness_2.Rdata"))
-
