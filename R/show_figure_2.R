06c9a343e104e291779e4546c0a8144aa7496803
sinaappeltauer <sina.appeltauer@tum.de>
2025-02-09 19:16:23 +0100

    Create show_figure_2.R

---
+++ R/show_figure_2.R
@@ -0,0 +1,42 @@
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Management Garchinger Heide restoration sites
+# Functional plant traits ####
+# Show figure 2
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Markus Bauer, Sina Appeltauer
+# 2025-01-28
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# A Preparation ###############################################################
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+### Packages ###
+library(here)
+library(tidyverse)
+library(patchwork)
+
+### Start ###
+rm(list = setdiff(ls(), c("graph_a", "graph_b", "graph_c", "graph_d")))
+
+
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# B Plot #######################################################################
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+(graph_a + graph_d + graph_b + graph_c) +
+  plot_layout(ncol = 2, guides = "keep") +
+  plot_annotation(tag_levels = "A", tag_prefix = "", tag_suffix = "") &
+  theme(plot.tag = element_text(size = 10, face = "bold"))
+
+### Save ###
+ggsave(
+  here("outputs", "figures", "figure_3_800dpi_17x15cm.tiff"),
+  dpi = 800, width = 17, height = 15, units = "cm"
+)
