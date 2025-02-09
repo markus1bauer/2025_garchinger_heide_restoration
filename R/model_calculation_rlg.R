268fcbdc25af69a3d1abf8cf05de80e9c285f212
sinaappeltauer <sina.appeltauer@tum.de>
2025-02-09 17:22:55 +0100

    Create model_calculation_rlg.R

---
+++ R/model_calculation_rlg.R
@@ -0,0 +1,107 @@
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Management Garchinger Heide restoration sites
+# Richness RLG ####
+# Model building
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# sina Appeltauer
+# 2025-01-28
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# A Preparation ###############################################################
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+### Packages ###
+library(here)
+library(tidyverse)
+library(ggbeeswarm)
+library(patchwork)
+library(brms)
+library(blme)
+library(DHARMa)
+
+### Start ###
+rm(list = ls())
+
+### Load data ###
+sites <- read_csv(
+  here("data", "processed", "data_processed_sites.csv"),
+  col_names = TRUE, na = c("na", "NA", ""), col_types =
+    cols(
+      .default = "?",
+      treatment = "f"
+    )
+) %>%
+  rename(y = rlg) %>%
+  filter(
+    is.na(location) | location != "Rollfeld") 
+
+
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# B Statistics #################################################################
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+## 1 Data exploration ##########################################################
+
+
+### a Graphs of raw data -------------------------------------------------------
+
+ggplot(sites, aes(y = y, x = treatment)) +
+  geom_quasirandom(color = "grey") + geom_boxplot(fill = "transparent")
+ggplot(sites, aes(y = y, x = cover_vegetation)) +
+  geom_quasirandom(color = "grey") + geom_smooth(method = "lm") +
+  facet_grid(~treatment)
+
+
+### b Outliers, zero-inflation, transformations? ----------------------------
+
+sites %>% group_by(treatment) %>% count(treatment)
+ggplot(sites, aes(x = treatment, y = y)) + geom_quasirandom()
+ggplot(sites, aes(x = y)) + geom_histogram(binwidth = .1)
+ggplot(sites, aes(x = y)) + geom_density()
+
+
+### c Check collinearity ------------------------------------------------------
+
+sites %>%
+  select(height_vegetation, cover_vegetation) %>%
+  GGally::ggpairs(lower = list(continuous = "smooth_loess")) +
+  theme(strip.text = element_text(size = 7))
+#--> exclude r > 0.7
+# Dormann et al. 2013 Ecography
+# https://doi.org/10.1111/j.1600-0587.2012.07348.x
+
+
+
+## 2 Model building ###########################################################
+
+
+### a Random structure ---------------------------------------------------------
+
+
+### b Fixed effects ------------------------------------------------------------
+
+m1 <- lm(
+  y ~ treatment,
+  data = sites
+)
+simulateResiduals(m1, plot = TRUE)
+
+m2 <- lm(
+  y ~ treatment * cover_vegetation,
+  data = sites
+)
+simulateResiduals(m2, plot = TRUE)
+
+
+### d Save ---------------------------------------------------------------------
+
+save(m1, file = here("outputs", "models", "model_richness_RLG_1.Rdata"))
+save(m2, file = here("outputs", "models", "model_richness_RLG_2.Rdata"))
+
