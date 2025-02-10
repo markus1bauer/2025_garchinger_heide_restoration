8c385296665e48e9ed5404c4ac9529ae1e17b97b
markus1bauer <markus1.bauer@tum.de>
2025-02-10 18:23:59 +0100

    Update figure 3

--- R/show_figure_3.R
+++ R/show_figure_3.R
@@ -30,13 +30,13 @@ rm(list = setdiff(ls(), c("graph_a", "graph_b", "graph_c")))
 
 
 
-graph_a + graph_b + graph_c +
+graph_a / graph_b / graph_c +
   plot_layout(guides = "keep") +
   plot_annotation(tag_levels = "A", tag_prefix = "", tag_suffix = "") &
   theme(plot.tag = element_text(size = 10, face = "bold"))
 
 ### Save ###
 ggsave(
-  here("outputs", "figures", "figure_3_800dpi_20x10cm.tiff"),
-  dpi = 800, width = 20, height = 10, units = "cm"
+  here("outputs", "figures", "figure_3_800dpi_8x26cm.tiff"),
+  dpi = 800, width = 8, height = 26, units = "cm"
   )
